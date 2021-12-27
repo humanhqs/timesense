7249c8ffc90234b6637fc7e056e5d1b2fdd5ab22
samatrouh <smatrouh@maservicesgroup.com.au>
2021-12-27 17:13:48 +1100

    Initial Commit

---
+++ Time Sense/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Time Sense
+//
+//  Created by Sam Matrouh on 27/12/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
